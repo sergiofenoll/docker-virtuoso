d0e4ecc2cabada76824d2f02055cb0330ba42c51
Niels Vandekeybus <progster@gmail.com>
2021-11-23 16:26:11 +0100

    add some maintenance scripts

---
+++ scripts/virtuoso/maintenance.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+USERNAME=${3:-"dba"}
+PASSWORD=${4:-"dba"}
+TRIPLESTORE=${2:-"triplestore"}
+COMMAND=$1
+
+if [[ "$#" -lt 1 ]]; then
+    echo "command is a required parameter"
+    exit -1
+fi
+
+case $COMMAND in
+    "checkpoint")
+        isql-v -H $TRIPLESTORE -U $USERNAME -P $PASSWORD <<EOF
+    exec('checkpoint');
+EOF
+        ;;
+    "vacuum")
+        isql-v -H $TRIPLESTORE -U $USERNAME -P $PASSWORD <<EOF
+    DB.DBA.vacuum();
+EOF
+        ;;
+    "dump_quads")
+        isql-v -H $TRIPLESTORE -U $USERNAME -P $PASSWORD <<EOF
+         dump_nquads ('dumps', 1, 10000000, 1);
+EOF
+        echo "dumped quads to data/db/dumps"
+        ;;
+    *)
+        echo "unrecognized command $COMMAND"
+        exit -1
+esac
